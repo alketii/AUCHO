9754f5202f8c193f309fcba1b56c3381a195370c
Alket Rexhepi <alketii@gmail.com>
2014-06-12 00:20:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2014  Alket Rexhepi info@kiwi.al
+Copyright (C) 2014  Alket Rexhepi
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
